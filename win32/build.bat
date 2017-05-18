007b28c624058048b185a00b6f70eb539708a5d7
Andrew Kallmeyer <fsmv@sapium.net>
2017-05-17 21:52:29 -0700

    Cleanups List:  - removed regex\ notes.txt  - fixed copyright year  - fixed weird new lines that got into win32/build.bat somehow

--- win32/build.bat
+++ win32/build.bat
@@ -1,98 +1,57 @@
 @echo off
 
-
 set EXEName=re.exe
-
 set SRC=..\code\win32_main.cpp
-
 set WindowsLibs=kernel32.lib user32.lib
 
-
-
 rem -------------------------------------
-
-
 
 rem Disables extra printing
 set CompilerOptions=-nologo
 
-
 rem Disables checking for minimal rebuild
-
 set CompilerOptions=%CompilerOptions% -Gm-
 
-
 rem Disables RTTI
-
 set CompilerOptions=%CompilerOptions% -GR-
 
-
 rem Disables inserting extra range check functions in the code (requires CRT)
-
 set CompilerOptions=%CompilerOptions% -GS-
 
-
 rem Disables exception handling
-
 set CompilerOptions=%CompilerOptions% -EHa-
 
-
 rem Don't put CRT references in the obj file
-
 set CompilerOptions=%CompilerOptions% -Zl
 
-
 rem Output full file paths paths in error messages
-
 set CompilerOptions=%CompilerOptions% -FC
 
-
 rem Enable all warnings and treat warnings as errors
-
 rem Disables unreferenced formal parameter warning
 set CompilerOptions=%CompilerOptions% /wd4100 -W4 -WX
 
-
 rem Put debug info in the obj file (don't make pdb files)
-
 set CompilerOptions=%CompilerOptions% -Z7
 
-
-
 rem We're a console app
-
 set LinkerOptions=-subsystem:console
 
-
 rem Don't link the CRT
-
 set LinkerOptions=%LinkerOptions% -nodefaultlib
 
-
 rem Disable incremental linking
-
 set LinkerOptions=%LinkerOptions% -incremental:no
 
-
 rem Remove functions and data that aren't referenced
-
 set LinkerOptions=%LinkerOptions% -opt:ref
 
-
 rem Provides __chkstk function, inserted to expand the stack if it goes past 1 page
-
 set LinkerOptions=%LinkerOptions% chkstk.obj
 
-
 rem Windows libs we use
-
 set LinkerOptions=%LinkerOptions% %WindowsLibs%
 
-
 rem -------------------------------------
-
-
-
-
 
 cl.exe %CompilerOptions% %SRC% -Fe%EXEName% /link %LinkerOptions%
